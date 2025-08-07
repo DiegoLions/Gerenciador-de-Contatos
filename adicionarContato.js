056a8befd15adc71fa883cc3cf12852736f9b776
DiegoLions <diegohsilva-2010@hotmail.com>
2025-08-07 12:42:36 -0300

    Update adicionarContato.js
    correções

--- adicionarContato.js
+++ adicionarContato.js
@@ -1,31 +1,43 @@
-const prompt = require('prompt-sync')()
-function adicionarContato (mainMenu, contatos) {
-const nome = prompt('Nome: ');
-const telefone = prompt('Telefone: ');
-const email = prompt('Email: ');
-const id = prompt('Digite um número para o ID do contato: ')
-    
-    if(isNaN(id) || id.trim() === '') {
-        console.log("ID inválido. Por favor, digite um número.");
-        return adicionarContato();
-    }
-    if(contatos.some(t => t.id === id)) {
-        console.log(`Já existe um contato com o ID ${id}. Por favor, escolha outro ID.`);
-        return adicionarContato();
-    }
-const adicionarNovoContato = prompt("Deseja adicionar um novo contato? (s/n)")
-    if(adicionarNovoContato.toLowerCase() =='s'){
-        adicionarContato();
-    } else{
-        const contato = { 
-            nome: nome,
-            telefone: telefone,
-            email: email,
-            id: id,
+const prompt = require('prompt-sync')();
+function adicionarContato(mainMenu, contatos) {
+    const nome = prompt('Nome: ');
+    let telefone;
+    while (true) {
+        telefone = prompt('Telefone: ');
+        if (isNaN(telefone) || telefone.trim() === '') {
+            console.log("Telefone inválido. Por favor, digite apenas números.");
+        } else {
+            break;
         }
-        contatos.push(contato)
-        console.log('Contato adicionado com sucesso!');
-        mainMenu();
+    }
+    const email = prompt('Email: ');
+    let id;
+    let numericId;
+    while (true) {
+        id = prompt('Digite um número para o ID do contato: ');
+        numericId = parseInt(id);
+        if (isNaN(numericId)) {
+            console.log("ID inválido. Por favor, digite um número.");
+        } else if (contatos.some(t => t.id === numericId)) {
+            console.log(`Já existe um contato com o ID ${numericId}. Por favor, escolha outro ID.`);
+        } else {
+            break;
+        }
+    }
+    const novoContato = {
+        nome: nome,
+        telefone: telefone,
+        email: email,
+        id: numericId,
     };
-    };
+    contatos.push(novoContato);
+    console.log('Contato adicionado com sucesso!');
+
+    const adicionarNovoContato = prompt("Deseja adicionar um novo contato? (s/n) ");
+    if (adicionarNovoContato.toLowerCase() === 's') {
+        return adicionarContato(mainMenu, contatos);
+    } else {
+        mainMenu(contatos);
+    }
+}
 module.exports = adicionarContato;
